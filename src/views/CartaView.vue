d8d8644487437f9b92bae9937a47a642b4e1755f
Gema Martin <geminway@live.com>
2022-04-23 21:51:24 +0200

    Added prices to the menu and modified the appearance of the menu.

--- src/views/CartaView.vue
+++ src/views/CartaView.vue
@@ -2,10 +2,30 @@
 <div class="carta-view">
 
     <div class="container-card" v-for="carta in cartasArray" :key="carta">
+        <table>
         <h1>{{carta.nameCarta}}</h1>
-        <ul v-for="food in carta.food" :key="food">
-            <li><span>{{food.nameFood}}</span> {{food.description}}</li>
-        </ul>
+            <tr v-if="carta.nameCarta === 'A palo seco'">
+                <th>Bebidas</th>
+                <th>UD</th>
+                <th>Botella</th>
+            </tr>
+            <tr v-else-if="carta.nameCarta !== 'Pá terminar'">
+                <th>Platos</th>
+                <th>Tapa</th>
+                <th>Ración</th>
+    
+            </tr>
+            
+            <tr v-else>
+                <th>Platos</th>
+                <th>Porción</th>
+            </tr>
+            <tr v-for="food in carta.food" :key="food">
+                <td><span>{{food.nameFood}}</span> {{food.description}}</td>
+                <td v-if="carta.nameCarta !== 'Pá terminar'" class="text-center">{{food.tapa || food.UD}}€</td>
+                <td v-if="food.racion || food.bottle"  class="text-center">{{food.racion || food.bottle + '€'}}</td>
+            </tr>
+        </table>
         <div class="link-pdf">
             <a :href="carta.link" target="_blank">Descargar la Carta con alérgenos</a>
         </div>
@@ -22,77 +42,77 @@ export default {
         return {
             cartasArray: [
                 { nameCarta: "El picoteo", food: [
-                        { nameFood: "Rasgueo", description: "patatas fritas" },
-                        { nameFood: "Escobilla", description: "patatas deluxe" },
-                        { nameFood: "Bulerías", description: "patatas bravas" },
-                        { nameFood: "Trémolo", description: "nachos, queso rallado y guacamole" },
-                        { nameFood: "Solo", description: "aceitunas" }
+                        { nameFood: "Rasgueo", description: "patatas fritas", tapa: 3.50, racion: 5.50  },
+                        { nameFood: "Escobilla", description: "patatas deluxe", tapa: 3.50, racion: 5.95 },
+                        { nameFood: "Bulerías", description: "patatas bravas", tapa: 3.50, racion: 5.95 },
+                        { nameFood: "Trémolo", description: "nachos, queso rallado y guacamole", tapa: 4.50, racion: 7.50 },
+                        { nameFood: "Solo", description: "aceitunas", tapa: 1.50, racion: '' }
                     ], link: "https://drive.google.com/file/d/1lRPLZ1hz1OrIbUVrvl2DHFH7UUt4yJw7/view?usp=sharing"
                 },
                 { nameCarta: "Lo light", food: [
-                        { nameFood: "Verdial", description: "ensaladilla rusa" },
-                        { nameFood: "Malagueña", description: "ensalada malagueña" },
-                        { nameFood: "Repiqueteo", description: "ensalada césar" },
-                        { nameFood: "Mariana", description: "ensalada de pipirrana " },
-                        { nameFood: "Rondeña", description: "ensalada con maíz, nueces, aguacate,tomate y parmesano" },
+                        { nameFood: "Verdial", description: "ensaladilla rusa", tapa: 2.95, racion: 4.50 },
+                        { nameFood: "Malagueña", description: "ensalada malagueña", tapa: 5.50, racion: 8.95 },
+                        { nameFood: "Repiqueteo", description: "ensalada césar", tapa: 4.50, racion: 7.50},
+                        { nameFood: "Mariana", description: "ensalada de pipirrana", tapa: 2.50, racion: 4.50},
+                        { nameFood: "Rondeña", description: "ensalada con maíz, nueces, aguacate,tomate y parmesano", tapa: 2.95, racion: 4.50},
                     ], link: "https://drive.google.com/file/d/1lRPLZ1hz1OrIbUVrvl2DHFH7UUt4yJw7/view?usp=sharing"
                 },
                 { nameCarta: "Lo andaluz", food: [
-                        { nameFood: "Jaleo", description: "ajo blanco con coco y piña" },
-                        { nameFood: "Pellizco", description: "croquetas de pollo" },
-                        { nameFood: "Contratiempo", description: "berenjenas con miel" },
-                        { nameFood: "Duende", description: "porra antequerana" },
-                        { nameFood: "Mirabrás", description: "gazpacho de frambuesa" },
-                        { nameFood: "A la flamenca", description: "bombón de sardina" },
-                        { nameFood: "Tientos", description: "pimiento del padrón asado" },
-                        { nameFood: "Fandangos", description: "boquerón en vinagre" },
-                        { nameFood: "Amalgama", description: "salchicha encebollada" },
-                        { nameFood: "Desplante", description: "lágrimas de pollo" },
+                        { nameFood: "Jaleo", description: "ajo blanco con coco y piña", tapa: 0.95, racion: '' },
+                        { nameFood: "Pellizco", description: "croquetas de pollo", tapa: 2.50, racion: 6.50 },
+                        { nameFood: "Contratiempo", description: "berenjenas con miel", tapa: 3.95, racion: 7.95 },
+                        { nameFood: "Duende", description: "porra antequerana", tapa: 1.50, racion: '' },
+                        { nameFood: "Mirabrás", description: "gazpacho de frambuesa", tapa: 0.50, racion: ''},
+                        { nameFood: "A la flamenca", description: "bombón de sardina", tapa: 0.50, racion: '' },
+                        { nameFood: "Tientos", description: "pimiento del padrón asado", tapa: 0.95, racion: 2.50},
+                        { nameFood: "Fandangos", description: "boquerón en vinagre", tapa: 1.50, racion: ''},
+                        { nameFood: "Amalgama", description: "salchicha encebollada", tapa: 1.50, racion: 3.95},
+                        { nameFood: "Desplante", description: "lágrimas de pollo", tapa: 2.50, racion: 6.50},
                     ], link: "https://drive.google.com/file/d/1x_hmQu7zTyurej-dCpxApm6JPla_K7aQ/view?usp=sharing",
                 },
                 { nameCarta: "Lo español", food: [
-                        { nameFood: "Cadencia", description: "alitas de pollo" },
-                        { nameFood: "A compás", description: "hummus" },
-                        { nameFood: "Olé", description: "tortilla de patata con cebolla" },
-                        { nameFood: "Alma", description: "brocheta de pollo, calabacín, tomate y cebolleta" },
-                        { nameFood: "Falseta", description: "brocheta de langostinos, tomate y cebolleta" },
-                        { nameFood: "Tonás", description: "buñuelos de merluza" },
-                        { nameFood: "Canastera", description: "hamburguesa de ternera o cresta picante" },
-                        { nameFood: "Liviana", description: "rollito de salmón y queso crema" },
-                        { nameFood: "Remate", description: "calamar frito" },
-                        { nameFood: "Redoble", description: "pincho de chistorra" },
+                        { nameFood: "Cadencia", description: "alitas de pollo", tapa: 3.95, racion: 7.95},
+                        { nameFood: "A compás", description: "hummus", tapa: 1.50, racion: 2.95},
+                        { nameFood: "Olé", description: "tortilla de patata con cebolla", tapa: 0.95, racion: ''},
+                        { nameFood: "Alma", description: "brocheta de pollo, calabacín, tomate y cebolleta", tapa: 2.95, racion: ''},
+                        { nameFood: "Falseta", description: "brocheta de langostinos, tomate y cebolleta", tapa: 3, racion: ''},
+                        { nameFood: "Tonás", description: "buñuelos de merluza", tapa: 1.50, racion: 5.95},
+                        { nameFood: "Canastera", description: "hamburguesa de ternera o cresta picante", tapa: 1.50, racion: ''},
+                        { nameFood: "Liviana", description: "rollito de salmón y queso crema", tapa: 2.50, racion: ''},
+                        { nameFood: "Remate", description: "calamar frito", tapa: 4.50, racion: 10.95},
+                        { nameFood: "Redoble", description: "pincho de chistorra", tapa: 1.95, racion: ''},
                     ], link: "https://drive.google.com/file/d/1x_hmQu7zTyurej-dCpxApm6JPla_K7aQ/view?usp=sharing",
                 },
                 { nameCarta: "Te lo recomiendo", food: [
-                        { nameFood: "Ida y vuelta", description: "pastela marroquí" },
-                        { nameFood: "Abanico", description: "tacos de cochinita pibil" },
-                        { nameFood: "Bastón", description: "uvas de queso de cabra" },
-                        { nameFood: "Capote", description: "turrón salado" },
-                        { nameFood: "Ayeo", description: "tartar de salchichón y miel de caña" },
-                        { nameFood: "Soniquete", description: "caramelo de rabo de toro" },
+                        { nameFood: "Ida y vuelta", description: "pastela marroquí", tapa: 1.95, racion: ''},
+                        { nameFood: "Abanico", description: "tacos de cochinita pibil", tapa: 2.95, racion: ''},
+                        { nameFood: "Bastón", description: "uvas de queso de cabra", tapa: 4.50, racion: ''},
+                        { nameFood: "Capote", description: "turrón salado", tapa: 2.50, racion: ''},
+                        { nameFood: "Ayeo", description: "tartar de salchichón y miel de caña", tapa: 5, racion: 8.50},
+                        { nameFood: "Soniquete", description: "caramelo de rabo de toro", tapa: 5.50, racion: ''},
                     ], link: "https://drive.google.com/file/d/1_hjqkK3NBDiTXsjvtzScI8nye40RNYIP/view?usp=sharing"
                 },
                 { nameCarta: "Pá terminar", food: [
-                        { nameFood: "Rumba", description: "tarta de queso" },
-                        { nameFood: "Toque", description: "coulant de chocolate y grosella" },
-                        { nameFood: "Pulso", description: "mousse de mango" },
-                        { nameFood: "Melisma", description: "arroz con leche" },
+                        { nameFood: "Rumba", description: "tarta de queso", racion: 1.95},
+                        { nameFood: "Toque", description: "coulant de chocolate y grosella", racion: 4.50},
+                        { nameFood: "Pulso", description: "mousse de mango", racion: 5.95},
+                        { nameFood: "Melisma", description: "arroz con leche", racion: 1.50},
                     ], link: "https://drive.google.com/file/d/1_hjqkK3NBDiTXsjvtzScI8nye40RNYIP/view?usp=sharing"
                 },
                 { nameCarta: "A palo seco", food: [
-                        { nameFood: "Agua" },
-                        { nameFood: "Refrescos" },
-                        { nameFood: "Redbul" },
-                        { nameFood: "Vino tinto Rioja" },
-                        { nameFood: "Vino tinto Ribiera del Duero \"Protos\"" },
-                        { nameFood: "Vino blanco Verdejo \"Protos\"" },
-                        { nameFood: "Vino Albariño Eidosea" },
-                        { nameFood: "San Miguel" },
-                        { nameFood: "Victoria" },
-                        { nameFood: "Alhambra verde 1995" },
-                        { nameFood: "Café e infusiones" },
-                        { nameFood: "Baileys" },
-                        { nameFood: "Combinados" },
+                        { nameFood: "Agua", UD: 1.50},
+                        { nameFood: "Refrescos", UD: 1.80},
+                        { nameFood: "Redbul" , UD: 2.50},
+                        { nameFood: "Vino tinto Rioja" , UD: 8.50, bottle: 67.50},
+                        { nameFood: "Vino tinto Ribiera del Duero \"Protos\"" , UD: 15, bottle: 120},
+                        { nameFood: "Vino blanco Verdejo \"Protos\"" , UD: 4.50, bottle: 35.50},
+                        { nameFood: "Vino Albariño Eidosea" , UD: 7.95, bottle: 60.50},
+                        { nameFood: "San Miguel" , UD: 2.50},
+                        { nameFood: "Victoria" , UD: 2.50},
+                        { nameFood: "Alhambra verde 1995" , UD: 2.95},
+                        { nameFood: "Café e infusiones" , UD: 1.80},
+                        { nameFood: "Baileys" , UD: 4},
+                        { nameFood: "Combinados" , UD: 'Desde 5.95'},
                     ], link: "https://drive.google.com/file/d/16DIiI8ej8L4OhOh09noeUIGeNqh9DqxK/view?usp=sharing"
                 },
             ]
@@ -103,10 +123,37 @@ export default {
 </script>
 
 <style scoped>
-ul{
-    list-style: none;
+ @font-face {
+     font-family: 'Ms Madi';
+     src: 
+     url(../../public/assets/fonts/MsMadi-Regular.ttf);
+ }
+
+ @font-face {
+     font-family: 'Indie Flower';
+     src: 
+     url(../../public/assets/fonts/IndieFlower-Regular.ttf);
+ }
+table{
     padding: 0 10px;
+    margin: .5em;
 }
+table h1{
+    font-family: 'Ms Madi', cursive;
+}
+
+th{
+    border-bottom: solid black 2px;
+    font-family: 'Ms Madi', cursive;
+     font-family: 'Indie Flower', cursive;
+    font-size: 1.5em;
+}
+
+td{
+    font-family: 'Indie Flower', cursive;
+    padding-right: 10px;
+}
+
 .carta-view{
     display: flex;
     flex-direction: column;
@@ -115,19 +162,17 @@ ul{
     margin: 1em 0;
 }
 
-
 .container-card{
+    display: flex;
+    flex-direction: column;
     background: #ffffff;
+    border: dashed #ef6962 5px;
     width: 90%;
     border-radius: 10px;
     height: min-content;
 }
 
-.container-card h1{
-    padding: 10px;
-}
-
-.container-card li span{
+.container-card span{
     color: #ef6962;
     font-weight: bold;
     font-size: 18px;
@@ -152,10 +197,17 @@ ul{
  background: pink;
 }
 
+.text-center{
+    text-align: center;
+    font-weight: bold;
+}
 @media screen and (min-width: 800px) {
     .container-card{
         width: 60%;
     }
 }
+
+
+
 
 </style>
